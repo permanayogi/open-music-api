ecfc0ae031a656d2e9e048dcc7bdd8ebbe044cdb
permanayogi <yogipermana597@gmail.com>
2023-01-13 22:25:05 +0700

    Initiate exceptions and validation

---
+++ src/validator/songs/schema.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const Joi = require('joi');
+
+const SongsPayloadSchema = Joi.object({
+  title: Joi.string().required(),
+  year: Joi.number().required(),
+  genre: Joi.string().required(),
+  performer: Joi.string().required(),
+  duration: Joi.number(),
+  albumId: Joi.string(),
+});
+
+module.exports = { SongsPayloadSchema };
