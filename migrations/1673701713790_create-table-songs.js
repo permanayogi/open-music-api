0746303600bfd85d90f9e8bbac6143c75f4a7094
permanayogi <yogipermana597@gmail.com>
2023-01-14 20:17:48 +0700

    create table songs with migrate

---
+++ migrations/1673701713790_create-table-songs.js
@@ -0,0 +1,39 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'integer',
+      notNull: true,
+    },
+    genre: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    duration: {
+      type: 'integer',
+      notNull: false,
+    },
+    album_id: {
+      type: 'VARCHAR(50)',
+      notNull: false,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+};
