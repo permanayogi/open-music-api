2993563cab27a00897c0a3e13834ee3feea939dc
permanayogi <yogipermana597@gmail.com>
2023-01-14 11:02:48 +0700

    created service and handler for albums and song

--- src/api/routes.js
+++ src/api/routes.js
@@ -4,25 +4,25 @@ const routes = (handler) => [
     // add albums data
     method: 'POST',
     path: '/albums',
-    handler: '',
+    handler: (request, h) => handler.postAlbumHandler(request, h),
   },
   {
     // get albums data by id
     method: 'GET',
     path: '/albums/{id}',
-    handler: '',
+    handler: (request, h) => handler.getAlbumById(request, h),
   },
   {
     // edit albums data by id
     method: 'PUT',
     path: '/albums/{id}',
-    handler: '',
+    handler: (request, h) => handler.putAlbumByIdHandler(request, h),
   },
   {
     // delete albums data by id
     method: 'DELETE',
     path: '/albums{id}',
-    handler: '',
+    handler: (request, h) => handler.deleteAlbumByIdHandler(request, h),
   },
 
   // routes for song
@@ -30,31 +30,31 @@ const routes = (handler) => [
     // add songs data
     method: 'POST',
     path: '/songs',
-    handler: '',
+    handler: (request, h) => handler.postSongHandler(request, h),
   },
   {
     // get all songs data
     method: 'GET',
     path: '/songs',
-    handler: '',
+    handler: () => handler.getSongsHandler(),
   },
   {
     // get songs data by id
     method: 'GET',
     path: '/songs/{id}',
-    handler: '',
+    handler: (request, h) => handler.getSongByIdHandler(request, h),
   },
   {
     // edit songs data by id
     method: 'PUT',
     path: '/songs/{id}',
-    handler: '',
+    handler: (request, h) => handler.putSongByIdHandler(request, h),
   },
   {
     // delete songs data by id
     method: 'DELETE',
     path: '/songs/{id}',
-    handler: '',
+    handler: (request, h) => handler.deleteSongByIdHandler(request, h),
   },
 ];
 
